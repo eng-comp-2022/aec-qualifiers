7687e724d7307973a72baefe23e4fdc8028a3a20
pwalsh2 <paul.N.walsh@outlook.ca>
2021-11-21 12:01:06 -0400

    not much

--- frontend/src/components/plotLand.js
+++ frontend/src/components/plotLand.js
@@ -2,15 +2,14 @@ import React from "react";
 import { Tile } from "./tile";
 
 export function PlotLand(props) {
-
+	// 10 x 10 array of tiles
 	let land_plot = Array.from(Array(10), () => new Array(10));
 
-	for (let i=0; i < 10; i++){
-		for (let j=0; j < 10; j++){
-			land_plot[i][j] = 8
+	for (let i = 0; i < 10; i++) {
+		for (let j = 0; j < 10; j++) {
+			land_plot[i][j] = 8;
 		}
 	}
-
 	const listStyles = {
 		float: "left",
 		padding: 0,
