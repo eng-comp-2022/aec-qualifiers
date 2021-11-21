9c2d836120491941b3f24e83d655196fc62663b6
Evan Fitzgerald <evandfitz@gmail.com>
2021-11-21 11:50:30 -0400

    update table

--- frontend/src/components/plotLand.js
+++ frontend/src/components/plotLand.js
@@ -2,56 +2,64 @@ import React from "react";
 import { Tile } from "./tile";
 
 export function PlotLand(props) {
-	// 10 x 10 array of tiles
-	var land_plot = [8, 8, 8, 8, 8, 8, 8, 8];
+
+	let land_plot = Array.from(Array(10), () => new Array(10));
+
+	for (let i=0; i < 10; i++){
+		for (let j=0; j < 10; j++){
+			land_plot[i][j] = 8
+		}
+	}
+
 	const listStyles = {
 		float: "left",
+		padding: 0,
 	};
 	return (
 		<div>
 			<ul style={listStyles}>
-				{land_plot.map(function (item) {
+				{land_plot[0].map(function (item) {
 					return <Tile key={item} ind={item}></Tile>;
 				})}
 			</ul>
 			<ul style={listStyles}>
-				{land_plot.map(function (item) {
-					return <Tile key={item} ind={item}></Tile>;
-				})}
-			</ul>
-
-			<ul style={listStyles}>
-				{land_plot.map(function (item) {
+				{land_plot[1].map(function (item) {
 					return <Tile key={item} ind={item}></Tile>;
 				})}
 			</ul>
 
 			<ul style={listStyles}>
-				{land_plot.map(function (item) {
+				{land_plot[2].map(function (item) {
 					return <Tile key={item} ind={item}></Tile>;
 				})}
 			</ul>
 
 			<ul style={listStyles}>
-				{land_plot.map(function (item) {
+				{land_plot[3].map(function (item) {
 					return <Tile key={item} ind={item}></Tile>;
 				})}
 			</ul>
 
 			<ul style={listStyles}>
-				{land_plot.map(function (item) {
+				{land_plot[4].map(function (item) {
 					return <Tile key={item} ind={item}></Tile>;
 				})}
 			</ul>
 
 			<ul style={listStyles}>
-				{land_plot.map(function (item) {
+				{land_plot[5].map(function (item) {
 					return <Tile key={item} ind={item}></Tile>;
 				})}
 			</ul>
 
 			<ul style={listStyles}>
-				{land_plot.map(function (item) {
+				{land_plot[6].map(function (item) {
+					return <Tile key={item} ind={item}></Tile>;
+				})}
+			</ul>
+
+			<ul style={listStyles}>
+				{land_plot[7].map(function (item) {
 					return <Tile key={item} ind={item}></Tile>;
 				})}
 			</ul>
